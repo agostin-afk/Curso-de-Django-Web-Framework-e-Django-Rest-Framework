8e692e8f1016cafc55eac6fa274469b106118f9e
Agostinho Ferreira <agostinho008.af45@gmail.com>
2026-08-14 11:53:01 -0300

    aula 27

--- aulas/sec5/recipes/views.py
+++ aulas/sec5/recipes/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'name': 'Agosto'}
+    return render(request, 'recipes/pages/home.html', context)
 
 
 def contato(request):
